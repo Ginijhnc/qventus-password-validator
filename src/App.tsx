4dbc943e069a214ea0d93995553a6d19d2ceae45
Ginijhnc <11620838+Ginijhnc@users.noreply.github.com>
2023-11-12 18:49:43 -0300

    style: base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import "./App.module.css";
+import styles from "./App.module.css";
+import { PasswordInput } from "@/components/PasswordInput/PasswordInput";
 
 export const App = () => {
   return (
-    <main>
+    <main className={styles.container}>
       <h1 data-testid="home-heading">Qventus Challenge</h1>
+      <PasswordInput />
     </main>
   );
 };
--- src/components/PasswordInput/PasswordInput.tsx
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -1,3 +1,25 @@
+import styles from "./PasswordInput.module.css";
+
+const Requirement = () => {
+  return (
+    <li className={styles.requirement}>
+      <span className={styles.requirementIndicator}>✔</span>
+      <span>requirement</span>
+    </li>
+  );
+};
+
 export const PasswordInput = () => {
-  return <input>test</input>;
+  return (
+    <div className={styles.passwordInputWrapper}>
+      <div className={styles.inputContainer}>
+        <input placeholder="Enter your password" className={styles.input} />
+      </div>
+      <ul className={styles.requirementsContainer}>
+        <Requirement />
+        <Requirement />
+        <Requirement />
+      </ul>
+    </div>
+  );
 };
